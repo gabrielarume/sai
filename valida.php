d4a03bdf0e3b9b0e36664e46e64ce6f8ff6c9057
gabrielarume <32935156+gabrielarume@users.noreply.github.com>
2019-03-23 03:00:46 -0600

    pruebs
    prueba

--- valida.php
+++ valida.php
@@ -1,4 +1,15 @@
 <?php
+ 
+ function validaRequerido($valor){
+	if(trim($valor) == ''){
+		return false;
+        }else{
+		return true;
+	  } 
+      
+
+        }
+
 
 function validaRequerido($valor){
 	if(trim($valor) == ''){
