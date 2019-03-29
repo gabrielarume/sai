dc14828092f277de9f7b4ac23326f075ff9733f5
gabrielarume <gabyruizmendieta@gmail.com>
2019-03-29 13:42:20 -0500

    UPTDATE:con ldap

--- valida.php
+++ valida.php
@@ -1,12 +1,26 @@
 <?php
-        $user =$_POST['user'];
-        $password =$_POST['password'];
-	echo $user;
+        #resivir los datos 
+        #$user =$_POST['user'];
+        #$password =$_POST['password'];
+	#echo $user;
 
+	#autentificacion LDAP
+	$ldap_dn = "uid=" .$_POST["user"].", dc=example,dc=com";
+	$ldap_password = $_POST["password"];
+
+	$ldap_con = ldap_connect("ldap.forumsys.com");
+	ldap_set_option($ldap_com, LDAP_OPT_PROTOCOL_VERSION, 3);
+
+	if (@ldap_bind($ldap_con, $ldap_dn, $ldap_password)){
+		echo "Inicio de sesión valida";
+	}else{ 
+		echo "Inicio de sesión no valida";
+ 	}
 
 	#$login =array(
 	#	'usuario'=> $user,
         #		'password'=> $password
-        #	);
+	#	);
+	#
 	#echo $login[$user];
 ?>
